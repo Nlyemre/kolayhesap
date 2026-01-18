c976b36870e673ab017102ded7f4483544ef37a0
emre <taykar96@gmail.com>
2026-01-19 00:51:59 +0300

    Değişiklik açıklaması

--- lib/Screens/anaekran_bilesenler/anaekran/anasayfa.dart
+++ lib/Screens/anaekran_bilesenler/anaekran/anasayfa.dart
@@ -581,7 +581,7 @@ class AppDrawer extends StatelessWidget {
 
   Widget _buildVersionInfo() {
     return const Text(
-      'Version 2.9.6',
+      'Version 2.9.9',
       style: TextStyle(fontSize: 10.0),
       textAlign: TextAlign.center,
     );
